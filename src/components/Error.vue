9cc371f116d1dad55b564357bad694b7c45c1883
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-08-06 09:19:03 +0200

    created an Error component that'll display an error message when data fetching fails

---
+++ src/components/Error.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  message: {
+    type: String,
+    default: "Data fetching failed, please check your network connection",
+  },
+});
+</script>
+
+<template>
+  <div class="mt-28 text-red-500 text-xl font-bold">
+    {{ message }}
+  </div>
+</template>
