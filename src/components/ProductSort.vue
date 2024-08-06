e76fc9400d49f69549d9393c95d1f8f47f26d1ad
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-08-06 09:10:29 +0200

    created the sort function that'll allow the user to sort the products between 3 options

---
+++ src/components/ProductSort.vue
@@ -0,0 +1,32 @@
+<script setup>
+ import { defineProps } from 'vue';
+  // import { ref } from 'vue';
+  // import { useStore } from 'vuex';
+  
+  const props = defineProps({onSortChange: {type: Function, required: true}})
+  // const store = useStore();
+  // const selectedSort = ref('default');
+  
+  const handleSort = (event) => {
+    props.onSortChange (event.target.value);
+  };
+  </script>
+
+  <template>
+    <div class="flex sm:w-[95%] max-w-[21rem] md:w-full">
+      <label for="sort" class="w-20 my-auto font-semibold">Sort by: </label>
+      <select
+        id="sort"
+        class="p-2.5 w-full max-w-[21rem] text-sm text-gray-900 bg-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+        @change="handleSort"
+        
+      >
+        <option value="default">Default</option>
+        <option value="lowToHigh">Price: Low to High</option>
+        <option value="highToLow">Price: High to Low</option>
+      </select>
+    </div>
+  </template>
+  
+
+  
